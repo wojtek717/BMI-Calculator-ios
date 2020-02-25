9c72d86773b09de62601e00ede020c81745f1000
wojtek717 <wojciechkonury@gmail.com>
2020-02-25 17:32:34 +0100

    display slider value

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -1,20 +1,29 @@
 //
 //  ViewController.swift
 //  BMI Calculator
-//
-//  Created by Angela Yu on 21/08/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
 
+    @IBAction func heightSlider(_ sender: UISlider) {
+
+        heightLabel.text = ((String(format: "%.2f", sender.value)) + "m")
+    }
+    
+    @IBAction func weightSlider(_ sender: UISlider) {
+        weightLabel.text = String(format: "%.0f", sender.value) + "m"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
